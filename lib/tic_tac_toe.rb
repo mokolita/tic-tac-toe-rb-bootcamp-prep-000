e7a878a2cca1e787421dbcce4d8bafbb49dacaa5
Meg Galvez <meg@oakmaine.com>
2019-03-13 16:54:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -80,3 +80,26 @@ end
     return nil
   end
 end
+
+def turn_count(board)
+  turns = 0 
+  
+  board.each do |position|
+    if (position == "X" || position == "O")
+      turns += 1 
+    else 
+    end 
+  end 
+ 
+  turns 
+end
+
+def current_player(board)
+  
+  if turn_count(board).even? 
+    return "X"
+  else turn_count(board).odd?
+    return "O"
+  end 
+
+end 
