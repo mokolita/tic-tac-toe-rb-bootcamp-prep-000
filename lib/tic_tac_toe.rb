181b0aa5b0e486d876204d5cba73c6e271259f5f
Meg Galvez <meg@oakmaine.com>
2019-03-13 16:56:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,6 +9,18 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
@@ -102,4 +114,39 @@ def current_player(board)
     return "O"
   end 
 
+end 
+
+
+
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def play(board)
+  turn = 0 
+  while turn < 9 
+    turn(board)
+    turn += 1 
+  end 
 end 
